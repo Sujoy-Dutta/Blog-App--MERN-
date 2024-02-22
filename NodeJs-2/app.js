312ca9e08babcda5985ee423908a8fc1d76a281a
Sujoy-Dutta <54211163+Sujoy-Dutta@users.noreply.github.com>
2024-02-22 23:16:30 +0530

    Add files via upload

---
+++ NodeJs-2/app.js
@@ -0,0 +1,97 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const multer = require('multer');
+
+const { createHandler } = require("graphql-http/lib/use/express");
+const expressPlayground = require('graphql-playground-middleware-express').default;
+
+const graphqlSchema = require('./graphql/schema');
+const graphqlResolver = require('./graphql/resolvers');
+
+const auth = require('./middleware/auth');
+// const feedRoutes = require('./routes/feed');
+// const authRoutes = require('./routes/auth');
+
+const app = express();
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    // const fileName = file.originalname.toLowerCase().split(' ').join('-');
+    // console.log("fileName", fileName);
+    cb(null, new Date().toISOString().replace(/:/g, '-') + '-' + file.originalname)
+    //cb(null, new Date().toISOString() + '-' + file.originalname);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); // application/json
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+);
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if(req.method === 'OPTIONS'){
+    return res.sendStatus(200);
+  }
+  next();
+});
+
+app.use(auth);
+
+app.use('/graphql', createHandler({
+  schema: graphqlSchema,
+  rootValue: graphqlResolver,
+  graphiql: true,
+  formatError(err){
+    if(!err.originalError){
+      return err;
+    }
+    const data= err.originalError.data;
+    const message = err.message || 'An error occured.';
+    const code = err.originalError.code || 500;
+    return { message: message, status: code, data: data };
+  }
+}))
+app.get('/playground', expressPlayground({ endpoint: '/graphql' }));
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data});
+});
+
+mongoose
+  .connect(
+    'mongodb+srv://sujoy_user1:sujoy_user1@cluster0.h8uwzta.mongodb.net/massages?retryWrites=true&w=majority'
+  )
+  .then(result => {
+    app.listen(8080);
+    })
+  .catch(err => console.log(err));
